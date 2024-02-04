0bff0c688835437d934853ac4c93c92892f9d5ff
federico-dibitonto <f.dibitonto94@gmail.com>
2024-02-05 00:25:10 +0100

    fixed style and try experiments

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const newAudio = document.querySelector("#soundtrack");
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+        newAudio.volume = 0.2;
+})
+
+// // const muteElement = document.querySelector("#mute");
+
+// // muteElement.addEventListener('click',
+// //     function(){
+// //         if (newAudio.volume > 0) {
+// //             newAudio.volume = 0;
+// //         } else {
+// //             newAudio.volume = 0.5;
+// //         }
+// //     }
+// // )
+
+// window.addEventListener("readystatechange", (event) => {
+//     console.log("page is fully loaded");
+//     newAudio.play();
+//     newAudio.volume = 0.2;
+//   });
